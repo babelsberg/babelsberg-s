f8a7c015fe41d1e607ed3b8080d7c524bda81bd9
Tim Felgentreff <timfelgentreff@gmail.com>
2014-08-05 11:06:52 +0200

    prepare and upload a green image

---
+++ tests/travis.sh
@@ -0,0 +1,20 @@
+$BUILDER_CI_HOME/testTravisCI.sh -verbose
+exitcode=$?
+
+if [ $exitcode -eq 0 ]; then
+    if [ "$ST" == "Squeak-4.5" ]; then
+	set -x
+	echo Uploading $BUILDER_CI_HOME/images/*.image
+	# cp $BUILDER_CI_HOME/images/*.image BabelsbergS.image
+	cd $PROJECT_HOME
+	echo $PWD
+	cp $BUILDER_CI_HOME/images/*.image BabelsbergS.image
+	cp $BUILDER_CI_HOME/images/*.changes BabelsbergS.changes
+	cp $BUILDER_CI_HOME/sources/*.sources .
+	echo "$VM_PATH/Linux/squeak -nosound -plugins "$VM_PATH/Linux" -encoding Latin1 -nodisplay BabelsbergS.image"
+	$VM_PATH/Linux/squeak -nosound -plugins "$VM_PATH/Linux" -encoding Latin1 -nodisplay BabelsbergS.image tests/build_image.st
+	echo "uploading..."
+	curl -T BabelsbergS.image http://www.lively-kernel.org/babelsberg/
+    fi
+fi
+exit $exitcode
